63871c7d8d08eba2dec9438daa814dd86015383b
huimin <2237501874@qq.com>
2020-07-06 09:44:41 +0800

    rabbitMQone

--- config-parent/config-server/src/main/java/cn/configServer/ConfigServerApp.java
+++ config-parent/config-server/src/main/java/cn/configServer/ConfigServerApp.java
@@ -3,9 +3,11 @@ package cn.configServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer//启动Config Server 功能
+@EnableEurekaClient
 public class ConfigServerApp {
     public static void main(String[] args) {
 
