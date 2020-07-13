6f2f43ae779aa14cfaaf4c1298b220f80ee33cbc
Huimin-12 <63449187+Huimin-12@users.noreply.github.com>
2020-07-13 18:27:32 +0800

    Update Eureka2App.java

--- Spring_cloud_parent/eureka_server2/src/main/java/cn/eureka/Eureka2App.java
+++ Spring_cloud_parent/eureka_server2/src/main/java/cn/eureka/Eureka2App.java
@@ -3,12 +3,13 @@ package cn.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//标注这是一个springboot项目
 @SpringBootApplication
 // 启用EurekaServer
 @EnableEurekaServer
 public class Eureka2App {
     public static void main(String[] args) {
+        
         SpringApplication.run(Eureka2App.class,args);
     }
 }
